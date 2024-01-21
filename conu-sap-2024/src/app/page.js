5aaef0f3ce9811a42d014f0a4ba3216be2bb297f
theTAOOO <tao.darren@gmail.com>
2024-01-20 22:42:57 -0500

    image move

--- conu-sap-2024/src/app/page.js
+++ conu-sap-2024/src/app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './App.css';
@@ -7,6 +7,20 @@ import './App.css';
 function App() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      setMousePosition({ x: clientX, y: clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -17,19 +31,21 @@ function App() {
     console.log(dateRange); // Replace with API call to backend
   };
 
+  const parallaxStyle = {
+    transform: `rotate(20deg) translateX(${(mousePosition.x / window.innerWidth) * 3 - 15}rem) translateY(${(mousePosition.y / window.innerHeight) * 3 - 20}rem)`,
+    opacity: 0.4,
+  };
+
   return (
     <div className="App">
-      <div className="welcome">welcome.</div>
-      <div className="wallpaper"></div>
-      <div className="calendar">
-        <p>Please select a date range</p>
-        <form onSubmit={handleSubmit}>
-          <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
-          <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-      
+      <p className="welcome">welcome.</p>
+      <div className="wallpaper" style={parallaxStyle}></div>
+      <form className="calendar" onSubmit={handleSubmit}>
+        <p>Please select a date range:</p>
+        <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+        <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
